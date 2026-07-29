3e7c707c3bf7b6e9b81186407a3d25c164daa850
Xumoneus <majhisumontos@gmail.com>
2026-07-29 21:27:05 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// WealthNest JavaScript
+
+console.log("Welcome to WealthNest!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const year = new Date().getFullYear();
+    console.log("Website loaded in " + year);
+});
